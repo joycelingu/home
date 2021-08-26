4e2378352064f3e516ccbdf1f6a61b82c7987f76
joycelingu <55028183+joycelingu@users.noreply.github.com>
2021-08-26 16:01:36 -0500

    Updates

---
+++ precache-manifest.4217f40d9af2968551e340c7035e8519.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df3338fa2d0029102f2cfbcc5a84262",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "dfe2fefdc35d7f313b48",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "88448ffcfce5fdaf8e09",
+    "url": "/home/static/js/2.cfafeff4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.cfafeff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe2fefdc35d7f313b48",
+    "url": "/home/static/js/main.92347268.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "ad243d0ed19cacbf0ee7313743fc2270",
+    "url": "/home/static/media/Oracle.ad243d0e.jpg"
+  },
+  {
+    "revision": "b3292a5fb982a8d044f135fb67e50e7d",
+    "url": "/home/static/media/profile.b3292a5f.JPG"
+  },
+  {
+    "revision": "e86b193c5676c742533eb4a82cc91606",
+    "url": "/home/static/media/resume.e86b193c.pdf"
+  },
+  {
+    "revision": "3b7acb94c66c139fa41e23a36aa0baa4",
+    "url": "/home/static/media/tamu.3b7acb94.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.4217f40d9af2968551e340c7035e8519.js"
 );
 
 self.addEventListener('message', (event) => {
